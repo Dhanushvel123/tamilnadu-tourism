608de86fdbcda738d19567d39f55536917b0fd61
Dhanushvel123 <vdhanush480@gmail.com>
2024-12-26 16:12:00 +0530

    updated

--- src/pages/ModernResort.js
+++ src/pages/ModernResort.js
@@ -305,8 +305,10 @@ const ModernResort = () => {
           <p className="lead">
             Your perfect vacation in Tamil Nadu is just a click away.
           </p>
-          <a href="/bookingpage" className="btn btn-light btn-lg rounded-pill mt-3">
-            Book Now <svg xmlns="http://www.w3.org/2000/svg" height="24px" style={{verticalAlign:'middle'}} viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-80v-100l120-84v-144L80-280v-120l320-224v-176q0-33 23.5-56.5T480-880q33 0 56.5 23.5T560-800v176l320 224v120L560-408v144l120 84v100l-200-60-200 60Z"/></svg>
+          <a href="/bookingpage" className="btn btn-outline-light btn-lg rounded-pill mt-3">
+            Book Now <svg xmlns="http://www.w3.org/2000/svg" style={{verticalAlign:'middle'}} width="24" height="24" fill="currentColor" class="bi bi-airplane-fill" viewBox="0 0 16 16">
+  <path d="M6.428 1.151C6.708.591 7.213 0 8 0s1.292.592 1.572 1.151C9.861 1.73 10 2.431 10 3v3.691l5.17 2.585a1.5 1.5 0 0 1 .83 1.342V12a.5.5 0 0 1-.582.493l-5.507-.918-.375 2.253 1.318 1.318A.5.5 0 0 1 10.5 16h-5a.5.5 0 0 1-.354-.854l1.319-1.318-.376-2.253-5.507.918A.5.5 0 0 1 0 12v-1.382a1.5 1.5 0 0 1 .83-1.342L6 6.691V3c0-.568.14-1.271.428-1.849"/>
+</svg>
           </a>
         </div>
       </section>
